84d45312ee421347146c28f3c09d530dbed041e6
Vivek Kadvani <vivek.k@simformsolutions.com>
2023-02-10 18:52:09 +0530

    remainning validation

--- delete.js
+++ delete.js
@@ -1,27 +1,55 @@
 const create = document.getElementById("btn_create")
-const update = document.getElementById("btn_Update")
+const update = document.getElementById("btn_Update_form")
 const p_name = document.getElementById("p_name")
 const prise = document.getElementById("p_prise")
+const p_image = document.getElementById("p_image")
 const description = document.getElementById("p_desc")
-const image = document.getElementById("p_image")
+
 if (localStorage.getItem("id") == undefined)
     localStorage.setItem("id", 0)
-try {
+
+
+    
     create.addEventListener("click", () => {
+            if (p_name.value == null && p_name.value=="") {
+                alert("enter name")
+                return 1;
+            }
+
+            else if (prise.value == null) {
+                alert("enter prise")
+                return 1;
+            }
+
+            else if (description.value == null) {
+                alert("enter Image Url")
+                return 1;
+            }
+
+            else if (p_image.value == null) {
+                alert("enter descrription")
+                return 1;
+            }
+            else 
+                create_product()
+        
+
+    })
+    function create_product() {
+        this.location.reload();
         let t_id = parseInt(localStorage.getItem("id")) + 1
+
         const product = {
             p_id: t_id,
             p_name: p_name.value,
             prise: prise.value,
             desc: description.value,
-            // image : image.value
+            image: p_image.value
         }
-
         var start = ""
         var end = ""
         var brk = "]"
         var str = localStorage.getItem("product")
-        console.log(str)
         if (str == null) {
             str = ""
             start = "["
@@ -30,18 +58,18 @@ try {
             start = ""
             end = ","
         }
+
         var n_str = str.substring(0, str.length - 1)
         localStorage.setItem("product", start + n_str + end + JSON.stringify(product) + brk)
         localStorage.setItem("id", t_id)
-    })
-}
-catch (e) { console.log(e) }
-const l_data = JSON.parse(localStorage.getItem("product"))
-// console.log(l_data[1])
-for (let i = 0; i < l_data.length; i++) {
+    }
 
+     
+const l_data = JSON.parse(localStorage.getItem("product"))
+
+for (let i = 0; i < l_data.length; i++) {
     document.getElementById("test").innerHTML += `<div id="p_card">
-    <div id="c_p_image"><img id="c_p_image_data" src="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXBwbGUlMjBsYXB0b3B8ZW58MHx8MHx8&w=1000&q=80"></div>
+    <div id="c_p_image"><img id="c_p_image_data" src="`+ l_data[i].image + `"></div>
     <div id="c_p_n_p">
         <div id="c_n">`+ l_data[i].p_name + `</div>
         <div id="c_p">`+ l_data[i].prise + `</div>
@@ -51,40 +79,27 @@ for (let i = 0; i < l_data.length; i++) {
     <a class="btn btn-danger" id="btn_delete" href="#?action=delete&id=`+ l_data[i].p_id + `">delete</a>
     <a class="btn btn-primary" id="btn_update" href="#?action=update&id=`+ l_data[i].p_id + `">Update</a>
 </div>`
-
 }
-
 
 update.addEventListener("click", () => {
     let id_url = this.document.URL
-    let id_val = parseInt(id_url.substring(id_url.length - 1, id_url.length))
-    console.log("update btn hit" + id_val)
+    let regex_id = /id=(\w+)/;
+    let match_id = id_url.match(regex_id);
+    let id_val_str = match_id ? match_id[1] : null;
+    let id_val = parseInt(id_val_str)
+    const l_data = JSON.parse(localStorage.getItem("product"))
 
-const l_data = JSON.parse(localStorage.getItem("product"))
     for (let i = 0; i < l_data.length; i++) {
         if (l_data[i].p_id == id_val) {
-            console.log("l_data[i].p_id : " + l_data[i].p_name)
-            console.log("id_val : " + id_val)
             const product = {
                 p_id: id_val,
                 p_name: p_name.value,
                 prise: prise.value,
                 desc: description.value,
-                // image : image.value
+                image: p_image.value
             }
-            console.log(product)
-            l_data[i]=product
-            console.log("before deelete :" + l_data[i].desc)
-            localStorage.setItem("product",JSON.stringify(l_data))
-
-
-            // if (l_data.length == 0) {
-            //     console.log("lenght 0")
-            //     localStorage.clear()
-            // }
-            // else
-            //     localStorage.setItem("product", JSON.stringify(l_data))
-            console.log("after deldete : " + l_data)
+            l_data[i] = product
+            localStorage.setItem("product", JSON.stringify(l_data))
             location.reload();
         }
     }
@@ -96,64 +111,34 @@ window.addEventListener('popstate', function (event) {
     let regex = /action=([^&]+)/;
     let match = id_url.match(regex);
     let action = match ? match[1] : null;
-    console.log(action);
-    let id_val = parseInt(id_url.substring(id_url.length - 1, id_url.length))
-    console.log(id_url + " : " + id_url.substring(id_url.length - 1, id_url.length))
+
+    let regex_id = /id=(\w+)/;
+    let match_id = id_url.match(regex_id);
+    let id_val_str = match_id ? match_id[1] : null;
+    let id_val = parseInt(id_val_str)
 
     if (action == "delete") {
         for (let i = 0; i < l_data.length; i++) {
             if (l_data[i].p_id == id_val) {
-                console.log("l_data[i].p_id : " + l_data[i].p_id)
-                console.log("id_val : " + id_val)
-                console.log("i : " + i)
-                console.log("before deelete :" + l_data)
                 l_data.splice(i, 1)
-                if (l_data.length == 0) {
-                    console.log("lenght 0")
+                if (l_data.length == 0)
                     localStorage.clear()
-                }
                 else
                     localStorage.setItem("product", JSON.stringify(l_data))
-                console.log("after deldete : " + l_data)
             }
         }
         this.location.reload();
-        console.log("delete hit")
     }
+
     else if (action == "update") {
-        console.log("update hit")
         for (let i = 0; i < l_data.length; i++) {
             if (l_data[i].p_id == id_val) {
-                console.log(l_data[i])
                 p_name.value = l_data[i].p_name
                 prise.value = l_data[i].prise
+                p_image.value = l_data[i].image
                 description.value = l_data[i].desc
                 update.disabled = false;
-                
-                // console.log("l_data[i].p_id : " + l_data[i].p_id)
-                // console.log("id_val : " + id_val)
-                // console.log("i : " + i)
-                // console.log("before deelete :" + l_data)
-                // l_data.splice(i, 1)
-                // if (l_data.length == 0) {
-                //     console.log("lenght 0")
-                //     localStorage.clear()
-                // }
-                // else
-                //     localStorage.setItem("product", JSON.stringify(l_data))
-                // console.log("after deldete : " + l_data)
             }
         }
-
-
-
-
     }
-
-
-
 });
-// const btn_del= document.getElementById("btn_del")
-// btn_del.addEventListener("click",()=>{
-//     console.log("clicked delete")
-// })
--- main.js
+++ main.js
@@ -1,63 +1,156 @@
+
 const create = document.getElementById("btn_create")
 const update = document.getElementById("btn_Update")
 const p_name = document.getElementById("p_name")
 const prise = document.getElementById("p_prise")
+const p_image = document.getElementById("p_image")
 const description = document.getElementById("p_desc")
 const image = document.getElementById("p_image")
+const btn_sort_id_a = document.getElementById("btn_sort_id_a")
+const btn_sort_id_d = document.getElementById("btn_sort_id_d")
+const btn_sort_name_a = document.getElementById("btn_sort_name_a")
+const btn_sort_name_d = document.getElementById("btn_sort_name_d")
+const btn_sort_price_a = document.getElementById("btn_sort_price_a")
+const btn_sort_price_d = document.getElementById("btn_sort_price_d")
+const nav_home = document.getElementById("nav_home")
+const nav_about = document.getElementById("nav_about")
+const nav_skill = document.getElementById("nav_skill")
+const nav_career = document.getElementById("nav_career")
+
+
 if (localStorage.getItem("id") == undefined)
     localStorage.setItem("id", 0)
-try{
-create.addEventListener("click", () => {
-  
 
-    this.location.reload(); 
+try {
+    create.addEventListener("click", () => {
+        function validate() {
+            if (p_name.value == null) {
+                alert("enter name")
+                return 1;
+            }
 
-    let t_id = parseInt(localStorage.getItem("id")) + 1
-    const product = {
-        p_id: t_id,
-        p_name: p_name.value,
-        prise: prise.value,
-        desc: description.value,
-        // image : image.value
+            else if (prise.value == null) {
+                alert("enter prise")
+                return 1;
+            }
+
+            else if (description.value == null) {
+                alert("enter Image Url")
+                return 1;
+            }
+
+            else if (p_image.value == null) {
+                alert("enter descrription")
+                return 1;
+            }
+        }
+
+    })
+    function create_product() {
+        this.location.reload();
+        let t_id = parseInt(localStorage.getItem("id")) + 1
+
+        const product = {
+            p_id: t_id,
+            p_name: p_name.value,
+            prise: prise.value,
+            desc: description.value,
+            image: p_image.value
+        }
+        var start = ""
+        var end = ""
+        var brk = "]"
+        var str = localStorage.getItem("product")
+        if (str == null) {
+            str = ""
+            start = "["
+        }
+        else {
+            start = ""
+            end = ","
+        }
+
+        var n_str = str.substring(0, str.length - 1)
+        localStorage.setItem("product", start + n_str + end + JSON.stringify(product) + brk)
+        localStorage.setItem("id", t_id)
     }
-    
-    var start=""
-    var end=""
-    var brk="]"
-    var str=localStorage.getItem("product")
-    if(str==null){
-        str=""
-        start="["
-    }
-    else{
-        start=""
-        end=","
-    }
-    var n_str=str.substring(0,str.length-1)
-    localStorage.setItem("product",start+n_str+end + JSON.stringify(product)+brk)
-    localStorage.setItem("id", t_id)
-    
-})
 
 }
-catch(e)
-{console.log(e)}
-finally{
-    
+
+
+catch (e) {
+    console.log(e)
 }
 
-const l_data=JSON.parse(localStorage.getItem("product"))
-console.log(l_data)
+const l_data = JSON.parse(localStorage.getItem("product"))
+
 for (let i = 1; i <= l_data.length; i++) {
-
-    document.getElementById("test").innerHTML+=`<div id="p_card">
-    <div id="c_p_image"><img id="c_p_image_data" src="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXBwbGUlMjBsYXB0b3B8ZW58MHx8MHx8&w=1000&q=80"></div>
+    document.getElementById("test").innerHTML += `<div id="p_card">
+    <div id="c_p_image"><img id="c_p_image_data" src="`+ l_data[i - 1].image + `"></div>
     <div id="c_p_n_p">
-        <div id="c_n">`+l_data[i-1].p_name+`</div>
-        <div id="c_p">`+l_data[i-1].prise+`</div>
+        <div id="c_n">`+ l_data[i - 1].p_name + `</div>
+        <div id="c_p">`+ l_data[i - 1].prise + `</div>
     </div>
-    <div id="c_p_desp">`+l_data[i-1].desc+`
+    <div id="c_p_desp">`+ l_data[i - 1].desc + `
     </div>
 </div>`
 
 }
+
+btn_sort_id_a.addEventListener("click", () => {
+    l_data.sort(function (a, b) {
+        return a.p_id - b.p_id;
+    });
+    localStorage.setItem("product", JSON.stringify(l_data))
+    location.reload();
+})
+
+btn_sort_id_d.addEventListener("click", () => {
+    l_data.sort(function (a, b) {
+        return a.p_id - b.p_id;
+    });
+    l_data.reverse()
+    localStorage.setItem("product", JSON.stringify(l_data))
+    location.reload();
+})
+
+btn_sort_name_a.addEventListener("click", () => {
+
+    l_data.sort(function (a, b) {
+        if (a.p_name < b.p_name) return -1;
+        if (a.p_name > b.p_name) return 1;
+        return 0;
+    });
+    localStorage.setItem("product", JSON.stringify(l_data))
+    location.reload();
+})
+
+btn_sort_name_d.addEventListener("click", () => {
+
+    l_data.sort(function (a, b) {
+        if (a.p_name < b.p_name) return -1;
+        if (a.p_name > b.p_name) return 1;
+        return 0;
+    });
+    l_data.reverse()
+    localStorage.setItem("product", JSON.stringify(l_data))
+    location.reload();
+})
+
+btn_sort_price_a.addEventListener("click", () => {
+    l_data.sort(function (a, b) {
+        return a.prise - b.prise;
+    });
+    localStorage.setItem("product", JSON.stringify(l_data))
+    location.reload();
+})
+
+
+btn_sort_price_d.addEventListener("click", () => {
+    l_data.sort(function (a, b) {
+        return a.prise - b.prise;
+    });
+    l_data.reverse();
+    localStorage.setItem("product", JSON.stringify(l_data))
+    location.reload();
+})
